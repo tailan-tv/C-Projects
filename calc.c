331abd2939c1701136ae7c2b7fb484604db04aa4
José Tailan Lopes <tailanlopes119@gmail.com>
2026-08-30 21:25:54 -0300

    calc.c

---
+++ calc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "");
+    
+	float num1, num2, result;
+	char operator;
+
+    printf("Digite o cálculo: ");
+    fflush(stdin);
+    scanf("%f %c %f", &num1, &operator, &num2);
+
+    if (operator == '+'){
+        result = num1 + num2;
+    }
+    else if (operator == '-'){
+        result = num1 - num2;
+    }
+    else if (operator == '/'){
+        result = num1 / num2;
+    }
+    else if (operator == '*'){
+        result = num1 * num2;
+    }
+    printf("O resultado e: %.2f",result);
+
+    return 0;
+}
